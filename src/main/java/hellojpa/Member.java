da3ac87c12e6a2fe3ce04844728cb178a794c790
haneul <tarabut@naver.com>
2023-12-15 17:16:27 +0900

    일대일(주 테이블에 외래키 양방향)

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -18,6 +18,10 @@ public class Member {
     @JoinColumn(name = "TEAM_ID")
     private Team team;
 
+    @OneToOne
+    @JoinColumn(name = "LOCKER_ID")
+    private Locker locker;
+
 
     // JPA는 기본 생성자가 있어야 한다.
     public Member(){}
